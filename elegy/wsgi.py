43ebc2494a025d61c3118e0d58b5946919671597
xisabao <ubiquitoustrees@gmail.com>
2015-05-31 16:05:24 -0700

    dealing with whitenoise

--- elegy/wsgi.py
+++ elegy/wsgi.py
@@ -8,7 +8,6 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
-from whitenoise.django import DjangoWhiteNoise
 from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
